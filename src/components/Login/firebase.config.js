ab007f7c7f3eafcb1caee876a8f3fb842c662c9a
sabbir-320 <sabbir195661@gmail.com>
2020-11-16 12:26:48 +0600

    added firebase auth

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBvLuIu5hKQeQKFvklgbzKrNmGQlclfSuY",
+  authDomain: "ema-jhon-site-6f0f7.firebaseapp.com",
+  databaseURL: "https://ema-jhon-site-6f0f7.firebaseio.com",
+  projectId: "ema-jhon-site-6f0f7",
+  storageBucket: "ema-jhon-site-6f0f7.appspot.com",
+  messagingSenderId: "949746127763",
+  appId: "1:949746127763:web:653a5e283de505fd0f8e66"
+};
+  export default firebaseConfig;
